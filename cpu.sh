c97caa0adf3518efef03f2b180789ebc0bc954da
cs.wang <wshokly@gmail.com>
2021-01-23 11:41:33 +0800

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#date="/var/scripts/`date +%Y%m%d`"
+#echo -e "--------- `date '+%Y-%m-%d %H:%M:%S'` ---------" >> $date.txt
+#echo -e "--mem" >> $date.txt
+#ps auxw|head -1 >> ${date}.txt;
+#ps auxw|sort -rn -k4|head -20 >> ${date}.txt;
+#sleep 2s
+#echo -e "--cpu" >> $date.txt
+#ps auxw|head -1 >> ${date}.txt;
+#ps auxw|sort -rn -k3|head -20 >> ${date}.txt;
+#echo -e "--top" >> $date.txt
+#top -n 1 -b | head -5 >> $date.txt
+#sleep 1s
+
+freemem=`free -m | grep -i mem| awk '{print int($4/$2*100)}'`
+usecpu=`top -n 1 -d 1 -b | grep Cpu | awk '{print int($2)}'`
+echo "freemem: $freemem, usecpu: $usecpu"
+if [ $freemem -le 20 -o $usecpu -ge 80 ] ;then
+    curl -s -o /dev/null  -d '{"msgtype":"text","text":{"content":"warning：IGGM cpu-used: '$usecpu'%, free-mem: '$freemem'%. "},"at":{"isAtAll":true}}' 'https://oapi.dingtalk.com/robot/send?access_token=0bc0e6c67ed1ea6207cbd65eff058278568c1889c5f7361fb918367d3a703476' -H 'Content-Type: application/json'
+fi
